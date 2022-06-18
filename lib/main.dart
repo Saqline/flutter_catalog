1ca34bc5a372509223dd1ba4efaf1ee92caf03ef
nur <imrannurint@gmail.com>
2022-06-19 00:28:00 +0600

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container(
+    
+
+    return MaterialApp(
+      home: HomePage(),
       
     );
   }
